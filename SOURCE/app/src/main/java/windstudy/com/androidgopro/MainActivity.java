b752f823cf8f65522db5f75e8cf3e99468d00883
thangnt <truongthangmt3@gmail.com>
2018-10-26 21:10:46 +0700

    add new layout

--- SOURCE/app/src/main/java/windstudy/com/androidgopro/MainActivity.java
+++ SOURCE/app/src/main/java/windstudy/com/androidgopro/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // day la dong code moi
+        
     }
 }
